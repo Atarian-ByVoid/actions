fc50e271e8cb550126a2dccefad1fb6140f4a9cb
Carlos <cgomespimentel7@gmail.com>
2024-11-23 10:50:28 -0300

    feat: Adiciona banner e sout

--- src/main/java/com/beto/actions/ActionsApplication.java
+++ src/main/java/com/beto/actions/ActionsApplication.java
@@ -8,6 +8,10 @@ public class ActionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ActionsApplication.class, args);
+
+		System.out.println("Isso foi um teste para o git huh actions");
+
+
 	}
 
 }
